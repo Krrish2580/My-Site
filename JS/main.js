a11941234554e5b8e6a6f41ad58548e3bf804dea
Krrish <krrishgaihre@gmail.com>
2020-08-02 10:09:47 +0545

    inner content responsive

--- JS/main.js
+++ JS/main.js
@@ -10,18 +10,6 @@ sidebarBtn.addEventListener('click', () => {
     }
 });
 
-const sidebar1Btn = document.querySelector('.responsive-sidebar-btn');
-let sidebar1Open = false;
-sidebar1Btn.addEventListener('click', () => {
-    if(!sidebar1Open) {
-        sidebar1Btn.classList.add('open');
-        sidebar1Open = true;
-    } else {
-        sidebar1Btn.classList.remove('open');
-        sidebar1Open = false;
-    }
-});
-
 function myFunction() {
     var x = document.getElementById("sidebar");
     if (x.style.display === "block") {
