3697374529cc396a1c7a3f60b5b16771735b72c0
Krrish2580 <krrishgaihre@gmail.com>
2020-08-30 09:24:28 +0545

    php file modified

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -1,15 +1,17 @@
 <?php
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
     $message = $_POST['message'];
 
     $email_from = 'krrishgaihre@gmail.com';
     $email_subject = "New message Received"
     $email_body = "User Name: $name. \n".
                     "User Email: $visitor_email. \n".
+                    "Subject: $subject. \n ".
                     "User message: $message.\n";
 
-    $to = "krrishgaihre@gmail.com";
+    $to = "sree.krishna2580@gmail.com";
 
     $headers = "From: $email_from \r\n";
 
